05e92b66b42243c275dfe4c5ecb38641cd6ec365
Driwell <driwell@tuta.io>
2024-11-13 16:32:08 +0000

    create base of ui

--- src/ui.rs
+++ src/ui.rs
@@ -4,10 +4,65 @@ pub struct Prompt;
 
 impl Plugin for Prompt {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, show_prompt);
+        app.add_systems(Startup, spawn_layout);
     }
 }
 
-fn show_prompt() {
-    println!("hello");
+fn spawn_layout(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let font = asset_server.load("fonts/FiraSans-Bold.ttf");
+
+    commands.spawn(Camera2dBundle::default());
+
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                display: Display::Grid,
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                grid_template_columns: vec![GridTrack::flex(1.0)],
+                grid_template_rows: vec![GridTrack::flex(1.0), GridTrack::flex(1.0)],
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            background_color: BackgroundColor(Color::BLACK),
+            ..default()
+        })
+        .with_children(|builder| {
+            builder
+                .spawn(NodeBundle {
+                    style: Style {
+                        display: Display::Grid,
+                        ..default()
+                    },
+                    ..default()
+                })
+                .with_children(|builder| {
+                    spawn_nested_text_bundle(builder, font.clone(), "prompt");
+                });
+            builder
+                .spawn(NodeBundle {
+                    style: Style {
+                        display: Display::Grid,
+                        ..default()
+                    },
+                    ..default()
+                })
+                .with_children(|builder| {
+                    spawn_nested_text_bundle(builder, font.clone(), "reply");
+                });
+        });
+}
+
+fn spawn_nested_text_bundle(builder: &mut ChildBuilder, font: Handle<Font>, text: &str) {
+    builder.spawn(
+        TextBundle::from_section(
+            text,
+            TextStyle {
+                font,
+                font_size: 24.0,
+                color: Color::WHITE,
+            },
+        )
+        .with_text_justify(JustifyText::Center),
+    );
 }
