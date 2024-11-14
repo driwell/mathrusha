3a69459fa2b2670945fe82af0765ac998372facd
Driwell <driwell@tuta.io>
2024-11-14 13:18:48 +0000

    get user input and show it on the reply box

--- src/ui.rs
+++ src/ui.rs
@@ -1,10 +1,22 @@
-use bevy::prelude::*;
+use std::mem;
+
+use bevy::{
+    input::{
+        keyboard::{Key, KeyboardInput},
+        ButtonState,
+    },
+    prelude::*,
+};
 
 pub struct Prompt;
+
+#[derive(Component)]
+struct Reply;
 
 impl Plugin for Prompt {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, spawn_layout);
+        app.add_systems(Update, listen_keyboard_input_events);
     }
 }
 
@@ -12,7 +24,7 @@ fn spawn_layout(mut commands: Commands, asset_server: Res<AssetServer>) {
     let font = asset_server.load("fonts/FiraSans-Bold.ttf");
 
     commands.spawn(Camera2dBundle::default());
-
+    // Top-level grid (app frame)
     commands
         .spawn(NodeBundle {
             style: Style {
@@ -28,41 +40,66 @@ fn spawn_layout(mut commands: Commands, asset_server: Res<AssetServer>) {
             ..default()
         })
         .with_children(|builder| {
-            builder
-                .spawn(NodeBundle {
-                    style: Style {
-                        display: Display::Grid,
+            builder.spawn(TextBundle {
+                text: Text::from_section(
+                    "prompt".to_string(),
+                    TextStyle {
+                        font: font.clone(),
+                        font_size: 100.0,
                         ..default()
                     },
+                )
+                .with_justify(JustifyText::Center),
+                ..default()
+            });
+
+            builder.spawn((
+                TextBundle {
+                    text: Text::from_section(
+                        "".to_string(),
+                        TextStyle {
+                            font: font.clone(),
+                            font_size: 100.0,
+                            ..default()
+                        },
+                    )
+                    .with_justify(JustifyText::Center),
                     ..default()
-                })
-                .with_children(|builder| {
-                    spawn_nested_text_bundle(builder, font.clone(), "prompt");
-                });
-            builder
-                .spawn(NodeBundle {
-                    style: Style {
-                        display: Display::Grid,
-                        ..default()
-                    },
-                    ..default()
-                })
-                .with_children(|builder| {
-                    spawn_nested_text_bundle(builder, font.clone(), "reply");
-                });
+                },
+                Reply,
+            ));
         });
 }
 
-fn spawn_nested_text_bundle(builder: &mut ChildBuilder, font: Handle<Font>, text: &str) {
-    builder.spawn(
-        TextBundle::from_section(
-            text,
-            TextStyle {
-                font,
-                font_size: 24.0,
-                color: Color::WHITE,
-            },
-        )
-        .with_text_justify(JustifyText::Center),
-    );
+fn listen_keyboard_input_events(
+    mut events: EventReader<KeyboardInput>,
+    mut edit_text: Query<&mut Text, With<Reply>>,
+) {
+    for event in events.read() {
+        if event.state == ButtonState::Released {
+            continue;
+        }
+
+        match &event.logical_key {
+            Key::Enter => {
+                let mut text = edit_text.single_mut();
+                if text.sections[0].value.is_empty() {
+                    continue;
+                }
+                let old_value = mem::take(&mut text.sections[0].value);
+                println!("{old_value}");
+            }
+
+            Key::Space => {
+                edit_text.single_mut().sections[0].value.push(' ');
+            }
+            Key::Backspace => {
+                edit_text.single_mut().sections[0].value.pop();
+            }
+            Key::Character(character) => {
+                edit_text.single_mut().sections[0].value.push_str(character);
+            }
+            _ => continue,
+        }
+    }
 }
